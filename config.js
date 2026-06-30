3b401eb81b98f8ee4e8c02129be097b7fb0f5124
Mike4160 <mike@elliottelec.com>
2026-06-30 16:19:04 -0400

    Update Supabase URL and anon key in config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // Fill these in from your Supabase project settings.
 // Supabase Dashboard -> Project Settings -> API
-window.MATERIAL_APP_SUPABASE_URL = "https://linwyigeiskvhueitttc.supabase.co";
-window.MATERIAL_APP_SUPABASE_ANON_KEY = "sb_publishable_cccJLt76Bg9nrG0RIsez_w_zAvsrmWT";
+window.MATERIAL_APP_SUPABASE_URL = "https://qdizyvlnimpfsuoiqcbh.supabase.co";
+window.MATERIAL_APP_SUPABASE_ANON_KEY = "sb_publishable_zHku8Ms_BlpJAdEDnCmvrg_JV3onX5G";
+
